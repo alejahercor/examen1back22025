217070a1c57c245149239f77303cbf527c187364
jessica <alejahercor@gmail.com>
2025-08-15 02:45:55 -0500

    correcciones modelos + config BD + evidencias

---
+++ examen1back22025-main/src/main/java/com/example/Examen1Back2/modelos/Curso.java
@@ -0,0 +1,55 @@
+package com.example.Examen1Back2.modelos;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "curso")
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_docente", referencedColumnName = "id")
+    @JsonBackReference(value = "docente-curso")
+    private Docente docente;
+
+
+    public Curso() {
+    }
+
+
+    public Curso(Integer id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Docente getDocente() {
+        return docente;
+    }
+
+    public void setDocente(Docente docente) {
+        this.docente = docente;
+    }
+}
